12d2b8d29801f9f5010db3dbe3ae2466458fb076
Kulesh Aleksandr <53787083+6mahdapihka9@users.noreply.github.com>
2021-10-16 10:40:47 +0300

    data update bug fixed

--- repositories/update.notes.data.js
+++ repositories/update.notes.data.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require('path');
 
 const updateNotesData = notes => {
-    fs.writeFileSync(path.join(__dirname, "../users.json"), JSON.stringify(notes));
+    fs.writeFileSync(path.join(__dirname, "../notes.json"), JSON.stringify(notes));
     console.log(notes.map(note => note.id + note.name));
 }
 
